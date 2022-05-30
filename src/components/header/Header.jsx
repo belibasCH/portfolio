656627d13b9761fe980e825b718856765e34a387
unknown <elias.braem@students.fhnw.ch>
2022-05-30 18:06:25 +0200

    add header content

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 function Header() {
   return (
-    <div>Header</div>
+   <header>
+     <div className="container header__container">
+       <h5>Hallo, Ich bin</h5>
+       <h1>Elias Bräm</h1>
+       <h5 className='text-light'>Frontend Developper</h5>
+      <CTA />
+     </div>
+   </header>
   )
 }
 
